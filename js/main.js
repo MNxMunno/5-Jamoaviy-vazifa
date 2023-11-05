df992f70ae8b8ff43fe136c24b6b71350794e415
MNxMunno <bilolhuzayfa@gmmail.com>
2023-11-05 18:02:14 +0500

    burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let burger = document.getElementById("burger_menu");
+
+burger.addEventListener("click", function () {
+  document.body.classList.toggle("open_menu");
+});
+
 let modeBtn = document.getElementById("dark-light");
 
 modeBtn.addEventListener("click", function () {
